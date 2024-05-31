3bc0a1bbeb8142c7d448c67c9559ba2b1c7103af
Illya <121565465+IllyaEpik@users.noreply.github.com>
2024-05-31 15:02:46 +0300

    modal window in shopPage

--- shop_page/views.py
+++ shop_page/views.py
@@ -5,8 +5,39 @@ import pandas
 from project.settings import DATABASE
 from registration_page.models import User, Product
 from .app import shop
+from PIL import Image
 def render_shop_page():
-
+    mod = False
+    type1 = None
+    id = None
+    if flask.request.method == "POST":
+        try:
+        
+            if flask.request.form["send"].split(";")[1] == "IMG":
+            
+                print(flask.request.form["send"])
+                print(os.path.abspath(__file__+"/../static/image"))
+                
+                img = flask.request.files.get('image')
+                print(img, type(img))
+                # image = Image.open(img)
+                # print(image, type(image))
+                # image.show()
+            elif flask.request.form["send"].split(";")[1] == "TEXT":
+                text = flask.request.form["text"]
+                id = flask.request.form["send"].split(";")[0]
+                for product in Product.query.all():
+                    if product.id == id:
+                        product.name = text
+        except :
+            try:
+                print(flask.request.form['img'])
+                id = flask.request.form['img']
+                type1 = "IMG"
+                
+            except:
+                pass
+            mod = True
     try:
         count =  len(flask.request.cookies.get('products').split(" "))
         if flask.request.cookies.get('products').split(" ")[0]== "":
@@ -48,7 +79,10 @@ def render_shop_page():
                               products = Product.query.all(),
                               count = count,
                               int = int,
-                              admin =admin
+                              admin =admin,
+                              type = type1,
+                              mod = mod,
+                              id = id
                               ))
     # cookie.set_cookie("all", count)
     
