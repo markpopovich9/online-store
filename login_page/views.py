ba28963b1b4a832b71f8844f4d69b8d105a311e9
markpopovich9 <121514610+markpopovich9@users.noreply.github.com>
2024-06-18 19:01:36 +0300

    6
    6

--- login_page/views.py
+++ login_page/views.py
@@ -8,14 +8,23 @@ def render_login_page():
     
     code=False
     if flask_login.current_user.is_authenticated:
-        return render_home_page()
+        return flask.redirect('/')
     else:
+        
         if flask.request.method == "POST":
             code="--> registration"
             for user in User.query.filter_by(login=flask.request.form['login']):
                 if user.password == flask.request.form['password']:
                     flask_login.login_user(user)
                     code=False
+                    return flask.redirect('/')
+            if  code:
+                for user in User.query.filter_by(email=flask.request.form['login']):
+                    # print(user)
+                    if user.password == flask.request.form['password']:
+                        flask_login.login_user(user)
+                        code = False
+                        return flask.redirect('/')
             
 
     return flask.render_template(
