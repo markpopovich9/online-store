8766d266400cbe447522c26e4b36f0ec8651b735
markpopovich9 <121514610+markpopovich9@users.noreply.github.com>
2024-06-18 18:59:52 +0300

    4
    4

--- home_page/views.py
+++ home_page/views.py
@@ -1,11 +1,16 @@
 import flask 
+import flask_login
 # import os
 # import json
 def render_home_page():
     # path_json = os.path.abspath(__file__ + "/../static/json/data.json")
     # with open(path_json, encoding= "utf-8") as file:
     #     read_data =  json.load(file)
-    
+    code=False
+    if flask_login.current_user.is_authenticated:
+        code=flask_login.current_user.login
+    print(code)
     return flask.render_template(
-        template_name_or_list= "home.html"
+        template_name_or_list= "home.html", 
+        code=code
     )
--- login_page/views.py
+++ login_page/views.py
@@ -1,17 +1,24 @@
 import flask 
 import flask_login
 from registration_page.models import User
-
+from home_page.views import render_home_page
 # import os
 # import json
 def render_login_page():
+    
+    code=False
     if flask_login.current_user.is_authenticated:
-        print("Hello World")
+        return render_home_page()
     else:
         if flask.request.method == "POST":
+            code="--> registration"
             for user in User.query.filter_by(login=flask.request.form['login']):
                 if user.password == flask.request.form['password']:
                     flask_login.login_user(user)
+                    code=False
+            
+
     return flask.render_template(
-        template_name_or_list= "login.html"
+        template_name_or_list= "login.html",
+        code=code
     )
